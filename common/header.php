ebfadf1d60b05631781711a09eaf79e98ace7dd1
Jeri Elizabeth <jeri.elizabeth@gmail.com>
2016-08-01 19:48:40 -0400

    initial work on creating a general use theme for SCRC

--- common/header.php
+++ common/header.php
@@ -24,6 +24,63 @@
 
     echo head_css();
     ?>
+
+    <?php
+    ($backgroundColor = get_theme_option('background_color')) || ($backgroundColor = "#FFFFFF");
+    ($textColor = get_theme_option('text_color')) || ($textColor = "#444444");
+    ($linkColor = get_theme_option('link_color')) || ($linkColor = "#888888");
+    ($buttonColor = get_theme_option('button_color')) || ($buttonColor = "#000000");
+    ($buttonTextColor = get_theme_option('button_text_color')) || ($buttonTextColor = "#FFFFFF");
+    ($titleColor = get_theme_option('header_title_color')) || ($titleColor = "#000000");
+    ?>
+    <style>
+        body {
+            background-color: <?php echo $backgroundColor; ?>;
+            color: <?php echo $textColor; ?>;
+        }
+        #site-title a:link, #site-title a:visited,
+        #site-title a:active, #site-title a:hover {
+            color: <?php echo $titleColor; ?>;
+            <?php if (get_theme_option('header_background')): ?>
+            text-shadow: 0px 0px 20px #000;
+            <?php endif; ?>
+        }
+        a:link {
+            color: <?php echo $linkColor; ?>;
+        }
+        a:visited {
+            color: <?php echo themeCustomizer_brighten($linkColor, 40); ?>;
+        }
+        a:hover, a:active, a:focus {
+            color: <?php echo themeCustomizer_brighten($linkColor, -40); ?>;
+        }
+        
+        .button, button,
+        input[type="reset"],
+        input[type="submit"],
+        input[type="button"],
+        .pagination_next a, 
+        .pagination_previous a {
+          background-color: <?php echo $buttonColor; ?>;
+          color: <?php echo $buttonTextColor; ?> !important;
+        }
+
+        #metadata-sidebar {
+            background-color: <?php echo  themeCustomizer_brighten($buttonColor, 40); ?>;
+        }
+        
+        .mobile li { 
+            background-color: <?php echo themeCustomizer_brighten($buttonColor, 40); ?>;
+        }
+        
+        .mobile li ul li {
+            background-color: <?php echo themeCustomizer_brighten($buttonColor, 20); ?>;
+        }
+        
+        .mobile li li li {
+            background-color: <?php echo themeCustomizer_brighten($buttonColor, -20); ?>;
+        }
+    </style>
     <!-- JavaScripts -->
     <?php queue_js_file('vendor/selectivizr', 'javascripts', array('conditional' => '(gte IE 6)&(lte IE 8)')); ?>
     <?php queue_js_file('vendor/respond'); ?>
@@ -38,28 +95,28 @@
         <header role="banner">
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
             <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
+        </header>
+        <nav>
+             <div id="primary-nav" role="navigation">
+                 <?php
+                      echo public_nav_main();
+                 ?>
+             </div>
 
-            <div id="search-container" role="search">
+             <div id="mobile-nav" role="navigation" aria-label="<?php echo __('Mobile Navigation'); ?>">
+                 <?php
+                      echo public_nav_main();
+                 ?>
+             </div>
+
+             <div id="search-container" role="search">
                 <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
                 <?php echo search_form(array('show_advanced' => true)); ?>
                 <?php else: ?>
                 <?php echo search_form(); ?>
                 <?php endif; ?>
             </div>
-        </header>
-
-         <div id="primary-nav" role="navigation">
-             <?php
-                  echo public_nav_main();
-             ?>
-         </div>
-
-         <div id="mobile-nav" role="navigation" aria-label="<?php echo __('Mobile Navigation'); ?>">
-             <?php
-                  echo public_nav_main();
-             ?>
-         </div>
-
+        </nav>
         <?php echo theme_header_image(); ?>
 
     <div id="content" role="main" tabindex="-1">
--- items/show.php
+++ items/show.php
@@ -1,38 +1,62 @@
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
 <div id="primary">
     <h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
+    <div id="main-block">
+
+      <div id="viewer-block">
+          <?php if (metadata('item', array('Item Type Metadata', 'Player'))): ?> 
+              <div class="item-player">
+                  <?php echo metadata('item', array('Item Type Metadata', 'Player')); ?>
+              </div>
+              <div id="transcription-window" class="element">
+                  <p><?php echo metadata('item', array('Item Type Metadata', 'Transcription')); ?></p>
+              </div>
+          <?php elseif (metadata('item', array('Item Type Metadata', 'Text'))): ?>
+              <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+              <div id="transcription-window" class="element">
+                  <h3><?php echo __('Annotations'); ?></h3>
+                  <?php echo metadata('item', array('Item Type Metadata', 'Text')); ?>
+              </div>
+          <?php else: ?>
+              <?php if (metadata('item', 'has files')): ?>
+                  <?php echo files_for_item(array('imageSize' => 'fullsize', 'imgAttributes' => array('class' => 'fullsize-image'))); ?>
+              <?php endif; ?>
+          <?php endif; ?>
+
+      </div>
+
+      <h3><?php echo __('Files'); ?></h3>
+      <div id="item-images">
+           <?php echo files_for_item(); ?>
+      </div>
+
+     <?php if(metadata('item','Collection Name')): ?>
+        <div id="collection" class="element">
+          <h3><?php echo __('Collection'); ?></h3>
+          <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
+        </div>
+     <?php endif; ?>
+
+       <!-- The following prints a list of all tags associated with the item -->
+      <?php if (metadata('item','has tags')): ?>
+      <div id="item-tags" class="element">
+          <h3><?php echo __('Tags'); ?></h3>
+          <div class="element-text"><?php echo tag_string('item'); ?></div>
+      </div>
+      <?php endif;?>
+
+      <!-- The following prints a citation for this item. -->
+      <div id="item-citation" class="element">
+          <h3><?php echo __('Citation'); ?></h3>
+          <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+      </div>
+         <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+    </div>
 
     <!-- Items metadata -->
-    <div id="item-metadata">
-        <?php echo all_element_texts('item'); ?>
+    <div id="metadata-sidebar">
+      <?php echo all_element_texts('item', array('show_element_sets' => 'Dublin Core')); ?>
     </div>
-
-    <h3><?php echo __('Files'); ?></h3>
-    <div id="item-images">
-         <?php echo files_for_item(); ?>
-    </div>
-
-   <?php if(metadata('item','Collection Name')): ?>
-      <div id="collection" class="element">
-        <h3><?php echo __('Collection'); ?></h3>
-        <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
-      </div>
-   <?php endif; ?>
-
-     <!-- The following prints a list of all tags associated with the item -->
-    <?php if (metadata('item','has tags')): ?>
-    <div id="item-tags" class="element">
-        <h3><?php echo __('Tags'); ?></h3>
-        <div class="element-text"><?php echo tag_string('item'); ?></div>
-    </div>
-    <?php endif;?>
-
-    <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
-    </div>
-       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
     <ul class="item-pagination navigation">
